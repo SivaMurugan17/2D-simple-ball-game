abbd2b2fb2c795245e9802ac908cd2989afbe7ab
Siva Murugan <atrssivamurugan@gmail.com>
2024-11-16 07:04:47 +0530

    add basic game setup
    with start scene, game scene, end scene

---
+++ constants.js
@@ -0,0 +1,11 @@
+
+export const GAME_SCENE_KEY = 'game-scene';
+export const BAT_KEY = 'bat';
+export const BALL_KEY = 'ball';
+export const BAT_SPEED = 200;
+export const BALL_SPEED = 250;
+
+export const CANVAS_SIZE = {
+    width : window.screen.width,
+    height : window.screen.height
+}
